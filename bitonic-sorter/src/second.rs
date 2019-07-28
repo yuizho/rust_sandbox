a53d5a04ae03f59bc7882dc03a0efaa835705eaf
yuizho <yuizho3@gmail.com>
2019-07-28 12:16:17 +0900

    modify second.rs to return Result

--- bitonic-sorter/src/second.rs
+++ bitonic-sorter/src/second.rs
@@ -1,10 +1,18 @@
 use super::SortOrder;
 
-pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) {
-    match *order {
-        SortOrder::Ascending => do_sort(x, true),
-        SortOrder::Descending => do_sort(x, false),
-    };
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+    if x.len().is_power_of_two() {
+        match *order {
+            SortOrder::Ascending => do_sort(x, true),
+            SortOrder::Descending => do_sort(x, false),
+        };
+        Ok(())
+    } else {
+        Err(format!(
+            "The length of  x is not a power of two. (x.len(): {}",
+            x.len()
+        ))
+    }
 }
 
 fn do_sort<T: Ord>(x: &mut [T], up: bool) {
@@ -43,7 +51,7 @@ mod tests {
     fn sort_u32_ascending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
-        sort(&mut x, &Ascending);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
 
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330])
     }
@@ -52,7 +60,7 @@ mod tests {
     fn sort_u32_descending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
-        sort(&mut x, &Descending);
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
 
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4])
     }
@@ -69,7 +77,7 @@ mod tests {
             "no",
             "GC",
         ];
-        sort(&mut x, &Ascending);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
         assert_eq!(
             x,
             vec![
@@ -97,7 +105,7 @@ mod tests {
             "no",
             "GC",
         ];
-        sort(&mut x, &Descending);
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
         assert_eq!(
             x,
             vec![
