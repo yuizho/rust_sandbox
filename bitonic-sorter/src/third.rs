991dbcb068055a48ecb94fb9c842157bac2ae7f5
yuizho <yuizho3@gmail.com>
2019-07-28 23:25:04 +0900

    add random vec generater for bitnic-sorter testing

--- bitonic-sorter/src/third.rs
+++ bitonic-sorter/src/third.rs
@@ -68,6 +68,7 @@ where
 mod tests {
     use super::{sort, sort_by};
     use crate::SortOrder::*;
+    use crate::utils::{new_u32_vec, is_sorted_ascending, is_sorted_descending};
 
     #[derive(Debug, PartialEq)]
     struct Student {
@@ -196,5 +197,19 @@ mod tests {
                 "GC"
             ]
         );
+    }
+
+    #[test]
+    fn sort_u32_large() {
+        {
+            let mut x = new_u32_vec(65536);
+            assert_eq!(sort(&mut x, &Ascending), Ok(()));
+            assert!(is_sorted_ascending(&x));
+        }
+        {
+            let mut x = new_u32_vec(65536);
+            assert_eq!(sort(&mut x, &Descending), Ok(()));
+            assert!(is_sorted_descending(&x));
+        }
     }
 }
