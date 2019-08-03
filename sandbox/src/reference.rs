75841e75d4afdb346de13e7d1efd4a48576192d9
yuizho <yuizho3@gmail.com>
2019-08-03 10:29:15 +0900

    refactor reference.rs in sand_box

--- sandbox/src/reference.rs
+++ sandbox/src/reference.rs
@@ -3,6 +3,19 @@ fn main() {
     let mut n = 0;
     println!("main: n={}", n);
 
+    fn f1(mut n: u32) {
+        n = 1;
+        println!("f1: n={}", n);
+    }
+
+    fn f2(n_ptr: &mut u32) {
+        println!("f2: n_ptr={}", n_ptr);
+
+        // *で参照先にアクセスできる。参照外しという。
+        *n_ptr = 2;
+        println!("f2: *n_ptr={}", *n_ptr);
+    }
+
     f1(n);
     println!("main: n={}", n);
 
@@ -20,17 +33,10 @@ fn main() {
 
     *n1_ptr = 1_000;
     assert_eq!(*n1_ptr, 1_000);
-}
 
-fn f1(mut n: u32) {
-    n = 1;
-    println!("f1: n={}", n);
-}
-
-fn f2(n_ptr: &mut u32) {
-    println!("f2: n_ptr={}", n_ptr);
-
-    // *で参照先にアクセスできる。参照外しという。
-    *n_ptr = 2;
-    println!("f2: *n_ptr={}", *n_ptr);
+    let mut c1 = 'A';
+    let c1_ptr = &mut c1;
+    assert_eq!(*c1_ptr, 'A'); //  参照外しによるポインタの指す値の取り出し
+    *c1_ptr = 'B'; // 参照外しによるポインタの指す値の書き換え
+    assert_eq!(c1, 'B');
 }
