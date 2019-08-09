3f8f9a1701215c06ef6660c206a9d48b580faed6
yuizho <yuizho3@gmail.com>
2019-08-09 22:05:06 +0900

    modify type_alias.rs in sandbox to add tuple-like struct sample

--- sandbox/src/types/type_alias.rs
+++ sandbox/src/types/type_alias.rs
@@ -10,6 +10,26 @@ fn main() {
 
     let id = 400;
     let now = 4567890123;
+    // これでも行けるけど、順番が変わるとエラーになってしまう
     let user = new_user(String::from("mika"), id, now);
+    println!("{:?}", user);
+
+    // タプル構造体を使った改善
+    #[derive(Debug)]
+    struct UserName2(String);
+    #[derive(Debug)]
+    struct Id2(i64);
+    #[derive(Debug)]
+    struct Timestamp2(i64);
+    type User2 = (Id2, UserName2, Timestamp2);
+
+    fn new_user2(name: UserName2, id: Id2, created: Timestamp2) -> User2 {
+        (id, name, created)
+    }
+
+    let id = Id2(400);
+    let now = Timestamp2(4567890123);
+    // idとnowを入れ替えるとコンパイルエラーとなる
+    let user = new_user2(UserName2(String::from("kazuki")), id, now);
     println!("{:?}", user);
 }
