48a5b2f449fe355310ef73300e96c36b6d0ed6f6
yuizho <yuizho3@gmail.com>
2019-08-18 01:14:19 +0900

    implement Lexical Analysis to parser

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,5 +1,30 @@
+use std::io;
+
+fn prompt(s: &str) -> io::Result<()> {
+    use std::io::{stdout, Write};
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    stdout.write(s.as_bytes())?;
+    stdout.flush()
+}
+
 fn main() {
-    println!("Hello, world!");
+    use std::io::{stdin, BufRead, BufReader};
+
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let stdin = BufReader::new(stdin);
+    let mut lines = stdin.lines();
+
+    loop {
+        prompt("> ").unwrap();
+        if let Some(Ok(line)) = lines.next() {
+            let token = lex(&line);
+            println!("{:?}", token);
+        } else {
+            break;
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -22,6 +47,7 @@ impl<T> Annot<T> {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum TokenKind {
     Number(u64),
     Plus,
@@ -57,6 +83,7 @@ impl Token {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum LexErrorKind {
     InvalidChar(char),
     Eof,
@@ -71,4 +98,102 @@ impl LexError {
     fn eof(loc: Loc) -> Self {
         LexError::new(LexErrorKind::Eof, loc)
     }
+}
+
+fn lex(input: &str) -> Result<Vec<Token>, LexError> {
+    let mut tokens = Vec::new();
+    let input = input.as_bytes();
+    let mut pos = 0;
+    macro_rules! lex_a_token {
+        ($lexer:expr) => {{
+            let (tok, p) = $lexer?;
+            tokens.push(tok);
+            pos = p;
+        }};
+    }
+    while pos < input.len() {
+        match input[pos] {
+            b'0'...b'9' => lex_a_token!(lex_number(input, pos)),
+            b'+' => lex_a_token!(lex_plus(input, pos)),
+            b'-' => lex_a_token!(lex_minus(input, pos)),
+            b'*' => lex_a_token!(lex_asterisk(input, pos)),
+            b'/' => lex_a_token!(lex_slash(input, pos)),
+            b'(' => lex_a_token!(lex_lparen(input, pos)),
+            b')' => lex_a_token!(lex_rparen(input, pos)),
+            b' ' | b'\n' | b'\t' => {
+                let ((), p) = skip_spaces(input, pos)?;
+                pos = p;
+            }
+            b => return Err(LexError::invalid_char(b as char, Loc(pos, pos + 1))),
+        }
+    }
+    Ok(tokens)
+}
+
+fn lex_plus(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    //match consume_byte(input, start, b'+') {
+    //    Ok((_, end)) => Ok(Token::plus(Loc(start, end)), end),
+    //    Err(err) => Err(err),
+    //}
+    consume_byte(input, start, b'+').map(|(_, end)| (Token::plus(Loc(start, end)), end))
+}
+fn lex_minus(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'-').map(|(_, end)| (Token::minus(Loc(start, end)), end))
+}
+fn lex_asterisk(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'*').map(|(_, end)| (Token::asterisk(Loc(start, end)), end))
+}
+fn lex_slash(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'/').map(|(_, end)| (Token::slash(Loc(start, end)), end))
+}
+fn lex_lparen(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'(').map(|(_, end)| (Token::lparen(Loc(start, end)), end))
+}
+fn lex_rparen(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b')').map(|(_, end)| (Token::rparen(Loc(start, end)), end))
+}
+fn lex_number(input: &[u8], mut pos: usize) -> Result<(Token, usize), LexError> {
+    use std::str::from_utf8;
+    let start = pos;
+    while pos < input.len() && b"1234567890".contains(&input[pos]) {
+        pos += 1;
+    }
+    let n = from_utf8(&input[start..pos]).unwrap().parse().unwrap();
+    Ok((Token::number(n, Loc(start, pos)), pos))
+}
+fn skip_spaces(input: &[u8], mut pos: usize) -> Result<((), usize), LexError> {
+    while pos < input.len() && b" \n\t".contains(&input[pos]) {
+        pos += 1;
+    }
+    Ok(((), pos))
+}
+
+fn consume_byte(input: &[u8], pos: usize, b: u8) -> Result<(u8, usize), LexError> {
+    if input.len() <= pos {
+        return Err(LexError::eof(Loc(pos, pos)));
+    }
+    if input[pos] != b {
+        return Err(LexError::invalid_char(
+            input[pos] as char,
+            Loc(pos, pos + 1),
+        ));
+    }
+    Ok((b, pos + 1))
+}
+
+#[test]
+fn test_lexer() {
+    assert_eq!(
+        lex("1 + 2 * 3 - -10"),
+        Ok(vec![
+            Token::number(1, Loc(0, 1)),
+            Token::plus(Loc(2, 3)),
+            Token::number(2, Loc(4, 5)),
+            Token::asterisk(Loc(6, 7)),
+            Token::number(3, Loc(8, 9)),
+            Token::minus(Loc(10, 11)),
+            Token::minus(Loc(12, 13)),
+            Token::number(10, Loc(13, 15)),
+        ])
+    )
 }
