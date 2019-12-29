66e4e520e0b780f85e5c52722bbfeff52448d34e
yuizho <yuizho3@gmail.com>
2019-12-29 20:32:37 +0900

    modify timer.rs to introduce termion

--- sandbox/src/thread/timer.rs
+++ sandbox/src/thread/timer.rs
@@ -1,8 +1,23 @@
+extern crate termion;
+
+use std::io::{stdout, Write};
 use std::time::Duration;
+use termion::raw::IntoRawMode;
+use termion::{clear, color};
 
 fn main() {
+    let mut stdout = stdout().into_raw_mode().unwrap();
     for duration in (1..1500).rev() {
-        println!("{}", convert_to_min(duration));
+        write!(
+            stdout,
+            "{}{}{}",
+            termion::cursor::Goto(2, 1),
+            clear::All,
+            convert_to_min(duration)
+        );
+        stdout.flush().unwrap();
+
+        // https://crates.io/crates/spin_sleep
         spin_sleep::sleep(Duration::from_secs(1));
     }
 }
